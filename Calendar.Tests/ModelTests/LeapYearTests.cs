a8197aa01683c84b35ab92f4280add8cb9e57e4a
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-09 15:35:49 -0700

    add test with meaningful failure for IsLeapYear_NumberDivisibleByFour_True

--- Calendar.Tests/ModelTests/LeapYearTests.cs
+++ Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -9,7 +9,7 @@ namespace Calendar.Tests
     [TestMethod]
     public void IsLeapYear_NumberDivisibleByFour_True()
     {
-      LeapYearTests testLeapYear = new LeapYear();
+      LeapYear testLeapYear = new LeapYear();
       Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
     }
   }
